34e8d3bd783f953a898529cb4d5e5a32086d5692
chayada0802 <78579013+chayada0802@users.noreply.github.com>
2021-06-13 17:29:11 +0700

    no message

---
+++ 333334.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace final3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+
+            do
+            {
+                input = Console.ReadLine();
+
+            } while (input!= "End");
+            string mode;
+            mode= Console.ReadLine();
+
+            switch (mode)
+            {
+                case "FindMax"
+:
+                
+                break
+               ;
+                case "FindMin"
+:
+                
+                break
+               ;
+                case "FindMean"
+:
+                
+                break
+               ;
+                default
+:
+                Console.WriteLine("Invalid mode");
+                    break;
+            }
+            }
+    }
+}
